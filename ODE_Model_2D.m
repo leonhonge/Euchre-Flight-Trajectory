69b62c1bceea329ba54a85a4e2dfa8bec8199a7c
Leon Honge <lhonge@umich.edu>
2026-09-10 19:21:26 -0400

    nothing works :(

--- ODE_Model_2D.m
+++ ODE_Model_2D.m
@@ -16,13 +16,13 @@ function [value, isterminal, direction] = ground(t, state)
     direction = -1;
 end
 
-function [value, isterminal, direction] = dive(t, state)
-    value = state(3) - 10000;
+function [value, isterminal, direction] = hold_speed(t, state)
+    value = state(3) - 7000;
     isterminal = 1;
     direction = -1;
 end
 
-function [value, isterminal, direction] = dive_end(t, state)
+function [value, isterminal, direction] = dive(t, state)
 
     [~, a, ~, ~] = atmoscoesa(0);
     impact_speed = 3*a;
@@ -39,14 +39,6 @@ function [value, isterminal, direction] = dive_end(t, state)
         -1    % descending through h = 0
     ];
 
-end
-function [value, isterminal, direction] = hold_speed(t, state)
-    [~, a, ~, ~] = atmoscoesa(0);
-    impact_speed = 3*a;
-
-    value = state(1) - impact_speed;
-    isterminal = 1;
-    direction = 1;
 end
 
 Mach_init = 8;
@@ -71,7 +63,7 @@ end
 
 state_0_cruise = state_climb(end, :);
 time_range_cruise = [t_climb(end), t_climb(end) + 240];
-options_cruise = odeset('Events', @dive);
+options_cruise = odeset('Events', @hold_speed);
 
 [t_cruise, state_cruise, te_cruise] = ...
     ode45(@ODE_cruise, ...
@@ -80,46 +72,35 @@ options_cruise = odeset('Events', @dive);
           options_cruise);
 
 if isempty(te_cruise)
-    error('Cruise never reached the 7000 m dive altitude.');
+    error('Cruise never reached the 7000 m hold speed altitude.');
 end
 
 
-state_0_dive = state_cruise(end, :);
+state_0_hold = state_cruise(end, :);
+time_range_hold = [t_cruise(end), t_cruise(end) + 240];
+options_hold = odeset('Events', @dive);
+
+[t_hold, state_hold, te_hold] = ...
+    ode45(@ODE_hold_velocity, ...
+    time_range_hold, ...
+    state_0_hold, ...
+    options_hold);
+
+if isempty(te_hold)
+    error('Hold is empty');
+end
 
 %point downwards to enter dive
-state_0_dive(2) = deg2rad(-30);
-time_range_dive = [t_cruise(end), t_cruise(end) + 240];
-options_dive = odeset('Events', @dive_end);
+state_0_dive = state_hold(end, :);
+state_0_dive(2) = deg2rad(-40);
+time_range_dive = [t_hold(end), t_hold(end) + 240];
+options_dive = odeset('Events', @ground);
 
-[t_dive, state_dive, te_dive, ye_dive, ie_dive] = ode45(@ODE_dive, time_range_dive, state_0_dive, options_dive);
-
-
-if ~isempty(ie_dive) && ie_dive(end) == 1
-
-    % Mach 3 was reached
-    state_0_hold = state_dive(end,:);
-
-    time_range_hold = ...
-        [t_dive(end), t_dive(end) + 240];
-
-    options_hold = odeset('Events', @ground);
-
-    [t_hold_speed, state_hold_speed] = ...
-        ode45(@ODE_hold_velocity, ...
-              time_range_hold, ...
-              state_0_hold, ...
-              options_hold);
-
-else
-
-    % Ground reached before Mach 3
-    t_hold_speed = [];
-    state_hold_speed = [];
-
-end
+[t_dive, state_dive, te_dive, ye_dive, ie_dive] = ... 
+    ode45(@ODE_dive, time_range_dive, state_0_dive, options_dive);
 
 
-if isempty(t_hold_speed)
+if isempty(te_dive)
 
     t_total = [ ...
         t_climb;
@@ -138,15 +119,15 @@ else
     t_total = [ ...
         t_climb;
         t_cruise(2:end);
+        t_hold(2:end)
         t_dive(2:end);
-        t_hold_speed(2:end)
     ];
 
     state_total = [ ...
         state_climb;
         state_cruise(2:end,:);
+        state_hold(2:end,:)
         state_dive(2:end,:);
-        state_hold_speed(2:end,:)
     ];
 
 end
@@ -166,12 +147,13 @@ plot(t_climb(end), state_climb(end,1), ...
 plot(t_cruise(end), state_cruise(end,1), ...
     'o', 'LineWidth', 2)
 
-plot(t_dive(end), state_dive(end,1), ...
-    'o', 'LineWidth', 2)
-if ~isempty(t_hold_speed)
-    plot(t_hold_speed(end), state_hold_speed(end,1), ...
+if ~isempty(t_hold)
+    plot(t_hold(end), state_hold(end,1), ...
         'o', 'LineWidth', 2)
 end
+
+plot(t_dive(end), state_dive(end,1), ...
+    'o', 'LineWidth', 2)
 
 xlabel('Time (s)')
 ylabel('Velocity (m/s)')
@@ -180,8 +162,8 @@ title('2D Trajectory Velocity')
 legend('Velocity', ...
        'Climb End', ...
        'Cruise End', ...
-       'Dive End', ...
-       'Hold End')
+       'Hold End', ...
+       'Dive End')
 
 hold off
 
@@ -200,13 +182,13 @@ plot(t_climb(end), rad2deg(state_climb(end,2)), ...
 plot(t_cruise(end), rad2deg(state_cruise(end,2)), ...
     'o', 'LineWidth', 2)
 
-plot(t_dive(end), rad2deg(state_dive(end,2)), ...
-    'o', 'LineWidth', 2)
-
-if ~isempty(t_hold_speed)
-    plot(t_hold_speed(end), state_hold_speed(end,1), ...
+if ~isempty(t_hold)
+    plot(t_hold(end), state_hold(end,1), ...
         'o', 'LineWidth', 2)
 end
+
+plot(t_dive(end), rad2deg(state_dive(end,2)), ...
+    'o', 'LineWidth', 2)
 
 xlabel('Time (s)')
 ylabel('Flight Path Angle (deg)')
@@ -215,8 +197,8 @@ title('2D Trajectory Flight Path Angle')
 legend('Flight Path', ...
        'Climb End', ...
        'Cruise End', ...
-       'Dive End', ...
-       'Hold End')
+       'Hold End', ...
+       'Dive End')
 
 hold off
 
@@ -235,13 +217,13 @@ plot(t_climb(end), state_climb(end,3), ...
 plot(t_cruise(end), state_cruise(end,3), ...
     'o', 'LineWidth', 2)
 
-plot(t_dive(end), state_dive(end,3), ...
-    'o', 'LineWidth', 2)
-
-if ~isempty(t_hold_speed)
-    plot(t_hold_speed(end), state_hold_speed(end,1), ...
+if ~isempty(t_hold)
+    plot(t_hold(end), state_hold(end,1), ...
         'o', 'LineWidth', 2)
 end
+
+plot(t_dive(end), state_dive(end,3), ...
+    'o', 'LineWidth', 2)
 
 xlabel('Time (s)')
 ylabel('Height (m)')
@@ -250,8 +232,8 @@ title('2D Trajectory Height')
 legend('Height', ...
        'Climb End', ...
        'Cruise End', ...
-       'Dive End', ...
-       'Hold End')
+       'Hold End', ...
+       'Dive End')
 
 hold off
 
@@ -270,13 +252,13 @@ plot(t_climb(end), state_climb(end,4), ...
 plot(t_cruise(end), state_cruise(end,4), ...
     'o', 'LineWidth', 2)
 
-plot(t_dive(end), state_dive(end,4), ...
-    'o', 'LineWidth', 2)
-
-if ~isempty(t_hold_speed)
-    plot(t_hold_speed(end), state_hold_speed(end,1), ...
+if ~isempty(t_hold)
+    plot(t_hold(end), state_hold(end,1), ...
         'o', 'LineWidth', 2)
 end
+
+plot(t_dive(end), state_dive(end,4), ...
+    'o', 'LineWidth', 2)
 
 xlabel('Time (s)')
 ylabel('Position (m)')
@@ -285,8 +267,8 @@ title('2D Trajectory Position')
 legend('Vehicle Position', ...
        'Climb End', ...
        'Cruise End', ...
-       'Dive End', ...
-       'Hold End')
+       'Hold End', ...
+       'Dive End')
 
 hold off
 
@@ -305,13 +287,13 @@ plot(state_climb(end,4), state_climb(end,3), ...
 plot(state_cruise(end,4), state_cruise(end,3), ...
     'o', 'LineWidth', 2)
 
-plot(state_dive(end,4), state_dive(end,3), ...
-    'o', 'LineWidth', 2)
-
-if ~isempty(t_hold_speed)
-    plot(t_hold_speed(end), state_hold_speed(end,1), ...
+if ~isempty(t_hold)
+    plot(t_hold(end), state_hold(end,1), ...
         'o', 'LineWidth', 2)
 end
+
+plot(state_dive(end,4), state_dive(end,3), ...
+    'o', 'LineWidth', 2)
 
 xlabel('Position (m)')
 ylabel('Height (m)')
@@ -320,7 +302,7 @@ title('Trajectory Simulation')
 legend('Trajectory', ...
        'Climb End', ...
        'Cruise End', ...
-       'Dive End', ...
-       'Hold End')
+       'Hold End', ...
+       'Dive End')
 
 hold off
